5be0209fb034cf78b3ae69aa5df635da7aec019d
sunusikiru <sunusihabibkasim6@gmail.com>
2026-06-02 16:30:11 +0100

    Create gradlew.bat

---
+++ gradlew.bat
@@ -0,0 +1,7 @@
+@rem Gradle wrapper script
+@if "%DEBUG%" == "" @echo off
+@rem Set local scope
+setlocal
+set DIRNAME=%~dp0
+set WRAPPER_JAR="%DIRNAME%gradle\wrapper\gradle-wrapper.jar"
+java -jar "%WRAPPER_JAR%" %*
